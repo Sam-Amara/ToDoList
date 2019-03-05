e209bec2ffa9e50bad2048f0f88f7ae907aa1f81
Sam Amara <amaramas@my.erau.edu>
2019-03-04 21:15:56 -0500

    Initial Commit

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -8,8 +8,20 @@ namespace ToDoList
 {
     class Program
     {
+        // This is an implementation of a To Do List using Simple Scanning 
+        // by Mark Forster 
         static void Main(string[] args)
         {
+            // Read or Create File
+            // Display main menu 1- View Task List 2- Add New Task 3- Exit
+
+            //1- Dispaly Task page 1, Enter Task# to select, > for next page, < for previous page, * for main menu
+
+            // # Select task complete and cross out or re-enter
+
+            //2- Add New Task, enter, confirm and save and back to Main menu
+
+            //3- Exit
         }
     }
 }
